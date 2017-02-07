2c9e0a793d3fd8850ec6cb062f05488a74baa6dd
Jacob Liou <jacob.c.liou@gmail.com>
2017-02-06 20:28:37 -0600

    Add files via upload

---
+++ MustangBanking.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mustangbanking;
+import java.util.*;
+import java.util.Scanner;
+/**
+ *
+ * @author jcl160030
+ */
+public class MustangBanking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+		boolean exit = false;
+		
+		while(!exit)
+		{
+		System.out.println("Welcome to your Mustang Bank! Please choose from the following options: ");
+		
+		int option = in.nextInt();
+		switch(option)
+		case 1:
+		
+		break;
+		case 2:
+		
+		break;
+		case 3:
+		
+		break;
+		case 4:
+		
+		break;
+		case 5:
+		
+		break;
+		case 6:
+		
+		break;
+		case 7:
+		
+		break;
+		case 8:
+		
+		break;
+		case 9:
+		exit = true;
+		break;
+		}
+		
+    }
+
+}
