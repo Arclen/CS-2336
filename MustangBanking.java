d3b8c0e0cb3decca7b28eb8f03844614e587ca0e
Jacob Liou <jacob.c.liou@gmail.com>
2017-02-08 20:30:50 -0600

    Update MustangBanking.java

--- MustangBanking.java
+++ MustangBanking.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package mustangbanking;
+
 import java.util.*;
 import java.util.Scanner;
-/**
- *
- * @author jcl160030
- */
 public class MustangBanking {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 		boolean exit = false;
