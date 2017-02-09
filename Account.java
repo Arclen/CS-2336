a8ceaa13646a3c3acd1b4941f3c353d52745c035
Jacob Liou <jacob.c.liou@gmail.com>
2017-02-08 20:54:36 -0600

    Update Account.java

--- Account.java
+++ Account.java
@@ -27,5 +27,20 @@ public class Account {
     void setInterestRate(double r)
     { interestRate = r; }
     
-    
+    void deposit(double dep)
+    { balance.setBalance(balance.getBalance() + dep); }
+  
+    void withdraw(double dep)
+    { 
+      if(balance.getBalance() < dep)
+        System.out.println("Insufficient funds available.");
+      else balance.setBalance(balance.getBalance() - dep); 
+    }
+  
+    void displayAccount()
+    {
+      System.out.println("Account ID: " + id +
+                         " \nCurrent Balance: " + balance.getBalance() +
+                         " \nInterest Rate: " + interestRate);
+    }
 }
