9ee3f3a797c08653fd36c8931f02a60ab1260a4b
Jacob Liou <jacob.c.liou@gmail.com>
2017-02-23 22:18:20 -0600

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -18,8 +18,10 @@ public class Assignment2 {
         int test[] = {3,6,4,1,3,4,2,5,3,0};
         Assignment2 MontelWilliams = new Assignment2();
         System.out.println(MontelWilliams.Solvable(0, test));
+        MontelWilliams.bubbleSort(test);
     }
     public Assignment2(){}
+    
     public boolean Solvable(int start, int[] squares)
     {
         int index = start;
@@ -54,5 +56,44 @@ public class Assignment2 {
         }
         return solved;
     }
+    
+    public void bubbleSort(int array[])
+{
+    System.out.println("Bubble Sort: \n");
+    boolean swap;
+    int temp;
+    System.out.println("Initial array: ");
+    for(int i=0; i<array.length; i++)
+            {
+                System.out.print(array[i]+" ");
+            }
+    System.out.println("\n");
+    do
+    {
+        swap = false;
+        for(int count = 0; count < (array.length-1); count++)
+        {
+            if (array[count] > array[count + 1])
+            {
+            temp = array[count];
+            array[count] = array[count + 1];
+            array[count + 1] = temp;
+            swap = true;
+            }
+//this loop outputs the array at the end of each traverse
+            for(int i=0; i<array.length; i++)
+            {
+                System.out.print(array[i]+" ");
+            }
+            System.out.print("\n");
+//this if statement outputs every swap that occurs
+            if(swap)
+            {
+                System.out.println("Numbers swapped: "+array[count+1]+" with "+array[count]);
+            }
+        }
+    } while (swap);
+    System.out.println("Sorted!"+"\n");
+}
     
 }
